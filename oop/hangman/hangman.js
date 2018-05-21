7e61dc6008715e36d3cade61c1ab89e561be91c9
minhdao <minhdao6@gmail.com>
2018-05-21 11:07:54 +0700

    move hangman.js to new folder

--- oop/hangman/hangman.js
+++ oop/hangman/hangman.js
@@ -16,7 +16,7 @@ let Hangman = function(word, guessCount) {
         });
         return puzzle;
     };
-    this.guess = (letter) => {
+    this.makeGuess = (letter) => {
         if (this.guessedLetters.indexOf(letter) === -1) {
             this.guessedLetters.push(letter);
         }
@@ -27,6 +27,6 @@ hangman1 = new Hangman('Zipper', 10);
 hangman2 = new Hangman('Mountain', 10);
 
 console.log(hangman1.word);
-hangman1.guess('z');
-hangman1.guess('p');
+hangman1.makeGuess('z');
+hangman1.makeGuess('p');
 console.log(hangman1.getPuzzle());
