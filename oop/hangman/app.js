9438cb8f37aeb925275be486379ae6175b3b96c8
minhdao <minhdao6@gmail.com>
2018-05-21 11:50:58 +0700

    display and add call to guess word

--- oop/hangman/app.js
+++ oop/hangman/app.js
@@ -5,7 +5,14 @@
 
 let hangman = new Hangman('Doggo', 4);
 
+// Start game - Display the word in *** form and number of guess a user has
+console.log(hangman.getPuzzle());
+console.log(hangman.guessCount);
+
+// Listen to user keypress
 window.addEventListener('keypress', (event) => {
     let guess = String.fromCharCode(event.charCode);
-    console.log(guess);
+    hangman.makeGuess(guess);
+    console.log(hangman.getPuzzle());
+    console.log(hangman.guessCount);
 });
