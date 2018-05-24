146edeefe94dd83a8bc5a37840f9b35e7cd5f8f8
minhdao <minhdao6@gmail.com>
2018-05-24 11:29:51 +0700

    boilerplate with babel setup

---
+++ boilerplate/input.js
@@ -0,0 +1,11 @@
+/**
+ * Babel is a tool that help to convert and make sure JS code you write will be
+ * compatable with other browsers
+ * input.js -> babel -> output.js(compatable accross different browsers)
+ */
+const name = 'Minh';
+console.log(name);
+
+class Person {
+
+}
---
+++ boilerplate/output.js
@@ -0,0 +1,15 @@
+'use strict';
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+/**
+ * Babel is a tool that help to convert and make sure JS code you write will be
+ * compatable with other browsers
+ * input.js -> babel -> output.js(compatable accross different browsers)
+ */
+var name = 'Minh';
+console.log(name);
+
+var Person = function Person() {
+  _classCallCheck(this, Person);
+};
