87e216b91773f3f0be00cac7b63dbda2f3e5ab98
minhdao <minhdao6@gmail.com>
2018-05-24 13:13:00 +0700

    use js module system to import and export function

--- boilerplate/src/index.js
+++ boilerplate/src/index.js
@@ -1,11 +1,18 @@
 /**
  * Babel is a tool that help to convert and make sure JS code you write will be
- * compatable with other browsers
+ * compatable with other browsers by changing development code into legacy old JS code
  * input.js -> babel -> output.js(compatable accross different browsers)
  */
-const name = 'Minh';
-console.log(name);
 
-class Person {
+/**
+ * Webpack is a tool that provides the JS module system to your code
+ * JS module system makes it easier to control dependencies by
+ *    Narrow down the scope of variables (no more global scope)
+ *    Introduce import and export statements
+ * Webpack also build and bundle for code in away that is good for browser performance
+ * input.js -> webpack -> output.js
+ */
 
-}
+import { add } from './utilities.js';
+
+console.log(add(10, 21));
---
+++ boilerplate/src/utilities.js
@@ -0,0 +1,8 @@
+/**
+ * [add return the sum of 2 numbers]
+ * @param {[number]} x [first number]
+ * @param {[number]} y [second number]
+ */
+export const add = (x, y) => {
+  return x + y;
+};
